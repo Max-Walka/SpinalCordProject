97de7554f24903e1097d0f57196068a6051258be
Max-Walka <walkermax193@gmail.com>
2026-03-19 14:45:50 +1300

    Landing page update
    added all the components for the landing page into the landingpage.tsx file

--- spinal-cord-assessment/src/app/landingPage/landingPage.tsx
+++ spinal-cord-assessment/src/app/landingPage/landingPage.tsx
@@ -1,10 +1,16 @@
 import RecentAssessments from "@/components/landing/recentAssessments";
 import Header from "@/components/layout/Header";
+import Buttons from "@/components/landing/Buttons";
+import UpcomingReviews from "@/components/landing/Upcoming";
+import Drafts from "@/components/landing/drafts";
 
 export default function LandingPage() {
   return (
     <div>
       <Header />
+      <Buttons />
+      <UpcomingReviews />
+      <Drafts />
       <RecentAssessments />
     </div>
   );
