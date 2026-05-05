8a8003df5efa06d0d30c904e7ea2a99ac3f0cd1c
Vrishab17 <chettyvrishi@gmail.com>
2026-05-06 10:27:24 +1200

    search/page.tsx put back in place

--- spinal-cord-assessment/src/app/search/page 3.tsx
+++
@@ -1,9 +0,0 @@
-import PatientSearch from "@/components/patients/PatientSearch";
-import Header from "@/components/layout/Header";
-
-export default function Page() {
-  return (<>
-  <Header />
-  <PatientSearch />
-  </>);
-}
