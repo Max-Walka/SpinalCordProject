08e5fd2fd29fc7290445e5a407913ad2399b78b5
Vrishab17 <chettyvrishi@gmail.com>
2026-05-17 01:17:54 +1200

    update file type for vecel deployment

--- spinal-cord-assessment/src/lib/exportAssessmentPdf.ts
+++ spinal-cord-assessment/src/lib/exportAssessmentPdf.ts
@@ -29,7 +29,9 @@ function get(obj: any, paths: string[]) {
 }
 
 function downloadPdf(bytes: Uint8Array, filename: string) {
-  const blob = new Blob([bytes], { type: "application/pdf" });
+  const blob = new Blob([bytes.buffer.slice(0) as ArrayBuffer], {
+    type: "application/pdf",
+  });
   const url = URL.createObjectURL(blob);
 
   const a = document.createElement("a");
