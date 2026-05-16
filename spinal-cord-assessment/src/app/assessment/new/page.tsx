809c618cb53b050ce5ab7c04690a1fb85eab6354
Vrishab17 <chettyvrishi@gmail.com>
2026-05-17 01:32:16 +1200

    removed imports in new/page.tsx

--- spinal-cord-assessment/src/app/assessment/new/page.tsx
+++ spinal-cord-assessment/src/app/assessment/new/page.tsx
@@ -1,34 +1,15 @@
-"use client";
-
+import { Suspense } from "react";
 import Header from "@/components/layout/Header";
 import AssessmentForm from "@/components/assessment/AssessmentForm";
-import AssessmentButtons from "@/components/assessment/AssessmentButtons";
+
 export default function AssessmentPage() {
   return (
     <div>
       <Header />
 
-      <div style={{ padding: "0px" }}>
+      <Suspense fallback={<div>Loading...</div>}>
         <AssessmentForm />
-      </div>
+      </Suspense>
     </div>
-
   );
-
-  const exportPDF = () => {
-    console.log("export");
-};
-
-const saveDraft = () => {
-    console.log("draft");
-};
-
-const saveFinal = () => {
-    console.log("final");
-};
-  <AssessmentButtons
-    onExportPDF={exportPDF}
-    onSaveDraft={saveDraft}
-    onSaveFinal={saveFinal}
-/>
 }
